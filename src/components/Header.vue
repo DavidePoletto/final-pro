d39d180028d5038eb77570e97dbcead916cd9c70
Davide Poletto <153924428+DavidePoletto@users.noreply.github.com>
2024-10-16 04:42:38 +0200

    header e inizio mainpage

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,12 +5,19 @@
     <div class="nav_container">
       <ul class="menu">
         <li><a href="">HOME</a></li>
-        <li><a href="">STORE</a></li>
+        <li><a href="">GAMES</a></li>
+        <li><a href="">CARD GAME</a></li>
         <li><a href="">NEWS</a></li>
         <li><a href="">EVENTS</a></li>
       </ul>
     </div>
-    <div class="login_container"></div>
+    <div class="login_container">
+      <div class="big_button"><button>DRASDFFD</button></div>
+      <div class="s_button">
+        <button>CIAO</button>
+        <button>CIAO</button>
+      </div>
+    </div>
   </header>
   </div>
 </template>
@@ -25,11 +32,12 @@ export default {
 .header_container {
   width: 100%;
   z-index: 100;
+  position: fixed;
 }
 
 .main-bar {
   height: 80px;
-  background-color: #323640;
+  background-color: #262930;
   border-bottom: 2px solid #f07935;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
   display: flex;
@@ -55,7 +63,7 @@ export default {
 
 .menu {
   display: flex;
-  justify-content: space-around;
+  justify-content: space-evenly;
   list-style-type: none;
   margin: 0;
   margin-top: 10px;
@@ -66,7 +74,7 @@ export default {
 .menu a {
   text-decoration: none;
   color: white;
-  font-weight: 600;
+  font-weight: 500;
   padding: 5px;
   display: block;
   position: relative;
@@ -91,5 +99,40 @@ export default {
 
 .login_container {
   flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.big_button {
+  width: 50%;
+  height: 40%;
+  display: flex;
+  justify-content: center;
+  margin-top: 10px;
+}
+
+.big_button button {
+  width: 80%;
+  color: rgb(0, 0, 0);
+  background-color: #f1752d;
+  border: none;
+  border-radius: 2px;
+}
+
+.s_button {
+  width: 50%;
+  height: 25px;
+  display: flex;
+  justify-content: center;
+  column-gap: 8px;
+  margin-top: 10px;
+}
+
+.s_button button {
+  width: 40%;
+  background-color: transparent;
+  border: 1px solid white;
+  color: white;
 }
 </style>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="page">
     <MainBar />
-    <div class="main_content"></div>
-    
+    <div class="main_content">
+      <div class="info_box">
+        <h1>GAMEVERSE</h1>
+        <h2>VIDEO GAME STORE</h2>
+        <p>Benvenuto su GAMEVERSE, qui potrai trovare una vasta scelta di videogames e card game, tuffati anche tu nel nostro fantastico mondo dio canaglia!!</p>
+        <div class="shop_button">
+          <button>SHOP NOW</button>
+          <button>GAYIOS</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -27,6 +36,63 @@ export default {
 
 .main_content {
   height: 100%;
-  background-color: gray;
+  background-image: url(./src/assets/IMG/sfondocopia.jpg);
+  background-size: cover;
+  background-position: center;
+  background-attachment: fixed;
+  height: 100vh;
+  margin-top: 80px;
+  position: relative;
+}
+
+.info_box {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: left;
+  color: white;
+  position: absolute;
+  top: 20px;
+  left: 50px;
+}
+
+
+.info_box h1 {
+  font-size: 4.5vw;
+  margin: 0;
+}
+
+.info_box h2 {
+  margin: 0;
+  font-size: 2.8vw;
+  font-weight: 400;
+  line-height: 0.3;
+}
+
+.info_box p {
+  width: 25%;
+  font-size: 1vw;
+  margin-top: 20px;
+  text-align: left;
+}
+
+@media (max-width: 768px) {
+  .info_box {
+    width: 80%; /* Riduci la larghezza su schermi piccoli */
+    top: 20px; /* Regola il posizionamento */
+    left: 20px;
+  }
+
+  .info_box h1 {
+    font-size: 7vw; /* Aumenta il ridimensionamento per piccoli schermi */
+  }
+
+  .info_box h2 {
+    font-size: 5vw;
+  }
+
+  .info_box p {
+    width: 100%; /* Adatta il paragrafo alla larghezza del box */
+  }
 }
 </style>
